4826a2b45eb01521c2a8c2b07f60afd07f8cc0e8
Wojciech Jarosz <jaroszw@gmail.com>
2021-04-20 08:45:46 +0200

    before image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Images from "../examples/Images"
 
 export default function Home() {
   return (
     <div>
       <h1>Home page!</h1>
+      <Images />
     </div>
   )
 }
